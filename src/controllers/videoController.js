70dfba1408386f39a288f336039f4b5f394ea46e
kamja44 <88872409+kamja44@users.noreply.github.com>
2022-12-23 22:14:26 +0900

    4.6
    Practice create Router

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,6 @@
 export const trending = (req, res) => res.send("Home Page Videos");
-export const watch = (req, res) => res.send("Watch Page");
+export const see = (req, res) => res.send("Watch Page");
 export const edit = (req, res) => res.send("Edit Page");
+export const search = (req, res) => res.send("Search Video");
+export const upload = (req, res) => res.send("Upload Video");
+export const deleteVideo = (req, res) => res.send("Delete Video");
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,8 +1,10 @@
 import express from "express";
-import { edit, Remove } from "../controllers/userController";
+import { edit, Remove, logout, see } from "../controllers/userController";
 const userRouter = express.Router();
 
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/remove", Remove);
+userRouter.get(":id", see);
 
 export default userRouter;
