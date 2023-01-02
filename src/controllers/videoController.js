31c18774e38bed6d0474735ce61c0d4892c2003c
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-01-02 17:36:40 +0900

    6.25
    Create Delete Page

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -65,3 +65,8 @@ export const postUpload = async (req, res) => {
 
   return res.redirect("/");
 };
+export const deleteVideo = async (req, res) => {
+  const { id } = req.params;
+  await Video.findByIdAndDelete(id);
+  return res.redirect("/");
+};
--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -5,6 +5,7 @@ import {
   postEdit,
   getUpload,
   postUpload,
+  deleteVideo,
 } from "../controllers/videoController";
 
 const videoRouter = express.Router();
@@ -12,5 +13,6 @@ const videoRouter = express.Router();
 videoRouter.get("/:id([0-9a-f]{24})", watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit);
 videoRouter.route("/upload").get(getUpload).post(postUpload);
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo);
 
 export default videoRouter;
