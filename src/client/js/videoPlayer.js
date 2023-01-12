fb11c9e34931d9cfd3d4ef022e1f7db1cfbde5a7
kamja44 <88872409+kamja44@users.noreply.github.com>
2023-01-12 12:36:20 +0900

    11.1
    create play btn

--- src/client/js/videoPlayer.js
+++ src/client/js/videoPlayer.js
@@ -1,1 +1,21 @@
-console.log("video player");
+const video = document.querySelector("video");
+const playBtn = document.getElementById("play");
+const muteBtn = document.getElementById("mute");
+const time = document.getElementById("time");
+const volume = document.getElementById("volume");
+
+const handlePlayClick = (event) => {
+  if (video.paused) {
+    video.play();
+  } else {
+    video.pause();
+  }
+};
+const handleMute = (event) => {};
+const handlePause = () => (playBtn.innerText = "Play");
+const handlePlay = () => (playBtn.innerText = "Pause");
+
+playBtn.addEventListener("click", handlePlayClick);
+muteBtn.addEventListener("click", handleMute);
+video.addEventListener("pause", handlePause);
+video.addEventListener("play", handlePlay);
